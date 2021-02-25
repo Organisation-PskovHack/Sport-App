4d96b7511907780b105e66cdcbb22beceff3361b
TabulaWeb <tabulaweb@mail.ru>
2021-02-25 12:36:01 +0300

    auth

--- section/urls.py
+++ section/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import *
 
 app_name = "section"
 
 urlpatterns = [
-
+    path("", homePage.as_view(), name="home"),
+    path("auth/", authPage.as_view(), name="home")
 ]
--- section/views.py
+++ section/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 
+class homePage(TemplateView):
+    template_name = "page/home.html"
+
+class authPage(TemplateView):
+    template_name = "page/auth.html"
